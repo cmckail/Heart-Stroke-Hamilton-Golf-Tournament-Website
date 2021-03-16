957172010c5c114381d24ceec429b9f6be7f4628
davejmelnyk@gmail.com <davejmelnyk@gmail.com>
2021-03-16 18:42:15 -0400

    modified testing and donate

--- client/pages/testing.tsx
+++ client/pages/testing.tsx
@@ -1,11 +1,13 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./components/checkoutForm";
+import NavigationBar from './components/navigationBar'
 
 const promise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 export default function Testing() {
   return (
     <div>
+               <NavigationBar/>
       <Elements stripe={promise}>
         <CheckoutForm />
       </Elements>
