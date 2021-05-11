f5ba3ceab03b47e830bbb72aa59e95625d49e2d3
David Melnyk <melnykd@HHSC.ca>
2021-05-11 16:23:04 -0400

    Added very basic registration success page

---
+++ client/pages/registration_success.tsx
@@ -0,0 +1,33 @@
+/*
+    Author: David Melnyk
+    This page serves as the donation page for the website.
+*/
+
+import React, { useState, useEffect, FormEvent } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import NavigationBar from "./components/navigationBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavigationBar />
+      <Head>
+        <title>DEVELOPMENT - Dan Segin Golf Tournament Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h2> Success! </h2>
+        <p>
+          {" "}
+          Order Number: 123 was successful!
+        </p>
+        <p>
+          Thank you for donating to the Heart and Stroke Foundation. Please note that all donations larger than $20 will receive a
+          charitable tax receipt. (Please allow up to 4 weeks for processing of
+          reciept)
+        </p>
+      </main>
+    </>
+  );
+}
